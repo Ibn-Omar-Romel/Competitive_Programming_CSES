a4aef6e5d452672faee9a730a206c56d191deba9
Ibn Omar Romel <ibnomarromel@gmail.com>
2025-07-11 01:30:52 +0600

    Update

--- Sorting and Searching/Towers.cpp
+++ Sorting and Searching/Towers.cpp
@@ -1,3 +1,5 @@
+
+
 #include<bits/stdc++.h>
 
 using namespace std;
@@ -5,12 +7,37 @@ using namespace std;
 #define ll long long int
 #define yes cout << "YES\n"
 #define no cout << "NO\n"
-#define vsort sort(vec.begin(), vec.end())
-#define asort sort(veca, veca+size )
+#define vsort sort( vec.begin(), vec.end())
+#define asort sort( veca, veca+size )
 #define ok cout << "ok" << endl
 #define nl cout << endl
 
+// loops
 #define fr(a, b) for(int i = a; i < b; i++) 
+
+
+void solve() {
+    int n;
+    cin >> n;
+
+    multiset<int>st;
+
+    for (int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+        
+
+        auto it = st.upper_bound(a);
+
+        if (it != st.end()) {
+            st.erase(it);
+        }
+        st.insert(a);
+    }
+
+    cout << st.size() << endl;
+
+}
 
 int main() {
 
@@ -18,31 +45,8 @@ int main() {
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int n;
-    cin >> n;
-    
-    vector<int>vec(n);
-    for (int i = 0; i < n; i++) {
-        cin >> vec[i];
-    }
-
-    map<int,int>mp;
-    int maxValue = 0;
-    for (int i = 0; i < n; i++) {
-        mp[vec[i]]++;
-        maxValue = max(maxValue, mp[vec[i]]);
-    }
-
-    if (n == 1) {
-        cout << 1 << endl;
-    }
-    else if (n >= 2 and maxValue == 1) {
-        cout << 2 << endl;
-    }
-    else {
-        cout << maxValue << endl;
-    }
-    
+    solve();
 
     return 0;
 }
+
